7c22a4f5d8c2c008deffec6aa25142183e00b6f6
oded <breiner@facebook.com>
2011-02-11 20:39:27 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mrquery::Application.config.secret_token = 'c8292151040d8ecb7bbc4de9534c0df5db9914a60f54ff1bf1314341fb0d8565aa0a29d1d2fc6d9d0df9cbe940abd686b34715cd66b34787d6da066652d6ea39'
